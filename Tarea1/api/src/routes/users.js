36b5a72344dbe19edc3f806982dbd80fe3e6fc76
JPEA97 <juanp.est97@gmail.com>
2024-02-29 12:26:57 -0600

    commiting full tarea1

---
+++ Tarea1/api/src/routes/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const UsersController = require('../controllers/users.controller');
+
+
+router.get('', UsersController.getUsers);
+router.get('/:id', UsersController.getUserById);
+
+
+module.exports = router;
